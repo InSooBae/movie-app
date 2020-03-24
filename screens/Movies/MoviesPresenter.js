7de0f9b6d7989371285f4b2f338af40735092038
InSooBae <qospwmf@gmail.com>
2020-03-24 18:03:28 +0900

    finish slider

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,40 +1,43 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-web-swiper';
+import { View, Dimensions } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 import Slide from '../../components/Movies/Slide';
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get('screen');
+
 const Container = styled.View`
   flex: 1;
   background-color: #1e2465;
   justify-content: center;
 `;
 
-const Section = styled.View`
-  background-color: red;
-  height: 100%;
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
 `;
-
-const Text = styled.Text``;
 
 export default ({ loading, nowPlaying }) => (
   <Container>
     {loading ? (
       <ActivityIndicator color="white" size="small" />
     ) : (
-      <>
-        <Swiper controlsEnabled={false} loop timeout={3}>
+      <SliderContainer>
+        <Swiper controlsEnabled={false} loop timeout={4}>
           {nowPlaying.map(movie => (
             <Slide
               key={movie.id}
               id={movie.id}
-              title={movie.original_language}
+              title={movie.original_title}
               overview={movie.overview}
               votes={movie.vote_average}
               backgroundImage={movie.backdrop_path}
+              poster={movie.poster_path}
             />
           ))}
         </Swiper>
-      </>
+      </SliderContainer>
     )}
   </Container>
 );
